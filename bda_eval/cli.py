9d3abf862722e1db1b6313d96aab3df7971148d5
Carlos Gil <carlos.h.gil@gmail.com>
2026-03-13 16:30:26 -0400

    eval app beta

---
+++ bda_eval/cli.py
@@ -0,0 +1,39 @@
+"""Command-line functionality."""
+
+import argparse
+
+
+def get_args() -> argparse.Namespace:
+    """Parse command-line arguments.
+
+    Returns:
+        Parsed command-line arguments.
+    """
+    bda_eval_desc = "Evaluate bda-svc reports."
+
+    parser = argparse.ArgumentParser(description=bda_eval_desc)
+
+    parser.add_argument(
+        "-r",
+        "--reference",
+        type=str,
+        required=True,
+        help=("Path to reference report folder."),
+    )
+
+    parser.add_argument(
+        "-p",
+        "--predicted",
+        type=str,
+        required=True,
+        help=("Path to predicted report folder."),
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help=("Path to output folder."),
+    )
+
+    return parser.parse_args()
